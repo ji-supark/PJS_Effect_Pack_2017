a8a6f63db61ec92e4b4d52aa33b12774e03dfb79
ji-supark <t_ieri@naver.com>
2016-11-26 02:10:16 +0900

    test

---
+++ Effect#1/Assets/script/rotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class rotation : MonoBehaviour {
+	public Vector3 rotatespeed;
+
+	void Update () 
+	{
+		transform.Rotate(rotatespeed * Time.deltaTime);
+	}
+}
