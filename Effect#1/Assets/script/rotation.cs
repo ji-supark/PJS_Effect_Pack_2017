ad533422d3704ba1e54459f4560d2ed8ccc30fb5
ji-supark <t_ieri@naver.com>
2016-12-10 16:34:55 +0900

    test

--- Effect#1/Assets/script/rotation.cs
+++ Effect#1/Assets/script/rotation.cs
@@ -4,9 +4,11 @@ using System.Collections;
 
 public class rotation : MonoBehaviour {
 	public Vector3 rotatespeed;
+	public Vector3 positionspeed;
 
 	void Update () 
 	{
 		transform.Rotate(rotatespeed * Time.deltaTime);
+		transform.Translate(positionspeed * Time.deltaTime);
 	}
 }
